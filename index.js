a5ed526ca1b54cd7542016d7846fef936cc6286f
Igor Kolomiyets <ikolomiyets@gmail.com>
2023-10-28 10:16:51 +0100

    Back to the call.then().catch() way of processing retreival request.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const core = require('@actions/core');
 const retrieve = require('./retrieve');
 
-try {
-    const serviceUrl = core.getInput('serviceUrl');
-    const apiToken = core.getInput('apiToken');
-    const stage = core.getInput('stage');
-    const artifactsString = core.getInput('artifacts');
+const serviceUrl = core.getInput('serviceUrl');
+const apiToken = core.getInput('apiToken');
+const stage = core.getInput('stage');
+const artifactsString = core.getInput('artifacts');
 
-    const response = await retrieve(serviceUrl, apiToken, stage, artifactsString);
-    core.setOutput('artifacts', response);
-} catch (error) {
+retrieve(serviceUrl, apiToken, stage, artifactsString)
+    .then(response => {
+        core.setOutput('artifacts', response);
+    })
+    .catch(error => {
     core.setFailed(error.message);
-}
+    })
